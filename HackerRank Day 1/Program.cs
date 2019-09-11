01cda68549f1d86c3ce5c0d8e30ea4423a1f3e84
MiliDas <milidas90@gmail.com>
2019-09-11 23:03:26 +0600

    HackerRank Day 1 code uploaded

---
+++ HackerRank Day 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HackerRank_Day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+
+        
+            // Declare second integer, double, and String variables.
+            int j;
+
+            double e;
+
+            string t;
+        
+            // Read and save an integer, double, and String to your variables.
+            j = int.Parse(Console.ReadLine());
+
+            e = Double.Parse(Console.ReadLine());
+
+            t = Console.ReadLine();
+
+            // Print the sum of both integer variables on a new line.
+            Console.WriteLine(i+j);
+        
+            // Print the sum of the double variables on a new line.
+            var u = string.Format("{0:0.0}", d+e);
+            Console.WriteLine(u);
+            
+            // Concatenate and print the String variables on a new line
+            // The 's' variable above should be printed first.
+            Console.WriteLine(s+t);
+        }
+    }
+}
