57cd45aaa0086ca735e83ddc253796d8070c567f
MiliDas <milidas90@gmail.com>
2019-09-12 16:37:30 +0600

    HackerRank Day 3 code uploaded

---
+++ HackerRank Day 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HackerRank_Day_3
+{
+    class Program
+    {
+        static void solve(int N) {
+            if(N%2!=0){
+                Console.WriteLine("Weird");
+            }
+            
+            if(N%2==0 && N >= 2 && N <= 5){
+                Console.WriteLine("Not Weird");
+            }
+            
+            if(N%2==0 && N >= 6 && N <= 20){
+                Console.WriteLine("Weird");
+            }
+
+            if(N%2==0 && N > 20){
+                Console.WriteLine("Not Weird");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            solve(N);
+        }
+    }
+}
