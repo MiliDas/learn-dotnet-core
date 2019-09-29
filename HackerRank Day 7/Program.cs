548d5165cb31dc806bb385c4ef2d2fddeb67d8d6
MiliDas <milidas90@gmail.com>
2019-09-29 17:12:52 +0600

    HackerRank Day 7 code upload

---
+++ HackerRank Day 7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HackerRank_Day_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+            ;
+            for(int i=n; i>0; i--){
+                Console.Write(arr[i-1]);
+                Console.Write(" ");
+            }
+            
+        }
+    }
+}
